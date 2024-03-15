ea02415cc5448aabae45c4ce36c26b09e6a89ae2
Jacob Ramsey-Smith <jrsrj13@gmail.com>
2024-03-14 21:26:31 -0500

    display top 10

--- main.py
+++ main.py
@@ -10,15 +10,15 @@ def get_pokemon_data(url):
         return None
 
 
-def get_fast_growth_pokemon(generation_name):
+def get_fast_growth_pokemon(generation_name, desired_growth_rate):
     weighted_moves_multiplier = 10
     fast_growth_pokemon = []
-    for i in range(1, 899):  # Assuming there are 898 Pokémon in the API
+    for i in range(1, 400):  # Assuming there are 898 Pokémon in the API
         pokemon_data = get_pokemon_data(f"https://pokeapi.co/api/v2/pokemon-species/{i}/")
         if pokemon_data:
             print(pokemon_data['name'])
             growth_rate = pokemon_data['growth_rate']['name']
-            if growth_rate == 'fast':  # Change this condition if you want different growth rates
+            if growth_rate == desired_growth_rate or not growth_rate:
                 weighted_moves_value = learns_moves_by_leveling_up(pokemon_data['id'], generation_name) * weighted_moves_multiplier
                 fast_growth_pokemon.append({"name": pokemon_data['name'], "weighted_value": weighted_moves_value})
     return fast_growth_pokemon
@@ -49,37 +49,41 @@ def get_base_stat_total(pokemon_name):
     if pokemon_data:
         base_stats = pokemon_data['stats']
         base_stat_total = sum(stat['base_stat'] for stat in base_stats)
+        if base_stat_total >= 600:
+            print(f"'{pokemon_name}' is excluded due to having too high of a BST- '{base_stat_total}'.")
+            return 0
         return base_stat_total
-    else:
-        return 0
+    return 0
 
 
 def find_highest_base_stat_total(fast_growth_pokemon):
-    max_base_stat_total = 0
-    best_pokemon = ''
-    total_calculated_value = 0
+    best_pokemon = []
     for pokemon in fast_growth_pokemon:
         base_stat_total = get_base_stat_total(pokemon["name"])
         calculated_value = base_stat_total + pokemon["weighted_value"]
-        if calculated_value > total_calculated_value:
-            max_base_stat_total = base_stat_total
-            total_calculated_value = calculated_value
-            best_pokemon = pokemon["name"]
-        elif total_calculated_value == calculated_value:
-            print(f"Score tie between '{best_pokemon}' and '{pokemon["name"]}' with a BST of '{base_stat_total}' "
-                  f"and total calculated value of '{calculated_value}'.")
-    return best_pokemon, max_base_stat_total, total_calculated_value
+        best_pokemon.append({"name": pokemon["name"], "bst": base_stat_total, "calculated_value": calculated_value})
+        best_pokemon = sorted(best_pokemon, key=lambda x: x["calculated_value"])
+        if len(best_pokemon) > 10:
+            best_pokemon.pop(0)
+    return best_pokemon
 
 
 if __name__ == "__main__":
     # Options for generation name are:
     # yellow, red-blue, gold-silver, crystal, ruby-sapphire, emerald, firered-leafgreen, etc
     generation_name = "emerald"
-    fast_growth_pokemon = get_fast_growth_pokemon(generation_name)
+    # Set growth rate to look for. If you don't want to filter by growth rate, then leave this blank
+    # Options for growth rate are:
+    # slow, medium-slow, medium, fast (This seems to be different from what's listed on bulbapedia)
+    growth_rate = "fast"
+    fast_growth_pokemon = get_fast_growth_pokemon(generation_name, growth_rate)
     if fast_growth_pokemon:
-        best_pokemon, max_base_stat_total, total_calculated_value = find_highest_base_stat_total(fast_growth_pokemon)
-        print(f"The Pokémon with the highest score among fast growth rate Pokémon is: {best_pokemon}")
-        print(f"Base stat total: {max_base_stat_total}")
-        print(f"Total score: '{total_calculated_value}'")
+        top_ten_pokemon = find_highest_base_stat_total(fast_growth_pokemon)
+        place = 10
+        for pokemon in top_ten_pokemon:
+            print(f"At number '{place}' is '{pokemon["name"]}'")
+            print(f"Base stat total: {pokemon["bst"]}")
+            print(f"Total score: '{pokemon["calculated_value"]}'\n")
+            place -= 1
     else:
         print("No fast growth rate Pokémon found that learn moves by leveling up.")
